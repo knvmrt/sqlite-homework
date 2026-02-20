b608168f5de234b65d5ec2931c74d87bdf0795e4
knvmrt <knvmrt@gmail.com>
2026-02-20 17:09:53 +0400

    Removed from the comment section.

--- app.sql
+++ app.sql
@@ -1,13 +1,13 @@
--- CREATE TABLE "Students" (
---     "uuid" TEXT PRIMARY KEY DEFAULT (hex(randomblob(5))),
---     "firstName" TEXT NOT NULL,
---     "lastName" TEXT NOT NULL,
---     "email" TEXT UNIQUE NOT NULL,
---     "phone" TEXT NOT NULL,
---     "dateBirth" DATE NOT NULL
--- );
+CREATE TABLE "Students" (
+    "uuid" TEXT PRIMARY KEY DEFAULT (hex(randomblob(5))),
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT UNIQUE NOT NULL,
+    "phone" TEXT NOT NULL,
+    "dateBirth" DATE NOT NULL
+);
 
--- INSERT INTO Students ("firstName", "lastName", "email", "phone", "dateBirth") 
--- VALUES ('Murad', 'Gahramanov', 'gahramanov.murad@outlook.com', '0514495422', '2003-10-05');
+INSERT INTO Students ("firstName", "lastName", "email", "phone", "dateBirth") 
+VALUES ('Murad', 'Gahramanov', 'gahramanov.murad@outlook.com', '0514495422', '2003-10-05');
 
--- SELECT * FROM "Students";
+SELECT * FROM "Students";
